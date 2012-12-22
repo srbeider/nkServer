f794cf319ed6b80a92b5f190d910602801e60457
srBeider <mcamps@gmx.com>
2012-12-22 18:00:31 +0100

    + WebClient + Router

---
+++ WebClient/WebClient/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Private()
+        {
+            return View();
+        }
+    }
+}
